3f147523c9dce63d7b2d4a9f45c0cdf1fc05fe4b
Vedang-15 <vedanghatekar@gmail.com>
2025-07-26 16:25:52 +0530

    Update production URL

--- Ecommerce-Project-with-Auth0-MockAI-Stripe-Azure/03-frontend/angular-digimart/src/environments/environment.prod.ts
+++ Ecommerce-Project-with-Auth0-MockAI-Stripe-Azure/03-frontend/angular-digimart/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  digimartApiUrl : "https://ecommerce-backend-api123.azurewebsites.net",
+  stripePublishableKey: "pk_test_51PbkcBRrJwFidErKzzlPLdV3yBYhQwvYSwIxApMIRQ9pFEAIn0Cfk7n54fzM77H7dpTgaynSQPz8s5VSxsycnyO800SPdu6VRz"
 };
