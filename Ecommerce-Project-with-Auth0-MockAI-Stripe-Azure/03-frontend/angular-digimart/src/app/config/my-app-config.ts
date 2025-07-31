307b0a77558442c736659f0a20c95b0e15571171
Vedang-15 <vedanghatekar@gmail.com>
2025-07-31 16:04:38 +0530

    update redirect_uri

--- Ecommerce-Project-with-Auth0-MockAI-Stripe-Azure/03-frontend/angular-digimart/src/app/config/my-app-config.ts
+++ Ecommerce-Project-with-Auth0-MockAI-Stripe-Azure/03-frontend/angular-digimart/src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
     domain: "dev-jh40eir8j1podnbf.us.auth0.com",
     clientId: "LHVHDrj6hhV0B1ke21R1avox2nDp9BNA",
     authorizationParams: {
-      redirect_uri: "https://gray-ocean-09ecb650f.1.azurestaticapps.net/login/callback",
+      redirect_uri: "https://gray-ocean-09ecb650f.1.azurestaticapps.net",
       audience: "http://localhost:8080",   // this is simply a unique identifier (audience) for the api we created in auth0 platform. Its has nothing to do with what port or env my backend is running on. i could have named it anything like "http://my-app.com" etc, but i chose this 8080 one as this si common and standard. This has nothing to do with backend server ports (8443, 9898)
     },
     cacheLocation: 'localstorage',
