b604a409e0adae78c64ee84973e95c68accae53d
Vedang-15 <vedanghatekar@gmail.com>
2025-07-31 15:28:15 +0530

    update config filw with new auth0 info

--- Ecommerce-Project-with-Auth0-MockAI-Stripe-Azure/03-frontend/angular-digimart/src/app/config/my-app-config.ts
+++ Ecommerce-Project-with-Auth0-MockAI-Stripe-Azure/03-frontend/angular-digimart/src/app/config/my-app-config.ts
@@ -1,9 +1,9 @@
 export default {
   auth: {
     domain: "dev-jh40eir8j1podnbf.us.auth0.com",
-    clientId: "deIKszh6PKQgLX8ClR0R55Rg3t6fJeze",
+    clientId: "LHVHDrj6hhV0B1ke21R1avox2nDp9BNA",
     authorizationParams: {
-      redirect_uri: "https://localhost:4200/login/callback",
+      redirect_uri: "https://gray-ocean-09ecb650f.1.azurestaticapps.net/login/callback",
       audience: "http://localhost:8080",   // this is simply a unique identifier (audience) for the api we created in auth0 platform. Its has nothing to do with what port or env my backend is running on. i could have named it anything like "http://my-app.com" etc, but i chose this 8080 one as this si common and standard. This has nothing to do with backend server ports (8443, 9898)
     },
     cacheLocation: 'localstorage',
@@ -18,6 +18,10 @@ export default {
       // QA backend endpoints (for staging testing, if needed)
       'https://localhost:9898/api/orders/**',
       'https://localhost:9898/api/checkout/purchase',
+
+      // prod endpoints
+      'https://ecommerce-backend-api123.azurewebsites.net/api/orders/**',
+      'https://ecommerce-backend-api123.azurewebsites.net/api/checkout/purchase'
     ],
   },
 }
